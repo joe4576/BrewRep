f8da78fde70bc0ded21d0ed1aaf0e53adf621401
Joe Brown <joebrown.4576@gmail.com>
2023-03-30 17:52:54 +0100

    Remove prisma seeding

--- server/prisma/canned/seed.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-import generateTenantData from "./tenants";
-
-const prisma = new PrismaClient();
-
-generateTenantData(prisma).then(() => {
-  // generate any other data
-});
