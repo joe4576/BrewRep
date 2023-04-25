a817bc4ad4b6733f8384ed6317a98d462372b8e6
Joe Brown <joebrown.4576@gmail.com>
2023-04-25 11:32:19 +0100

    Prevent redirect to login on page reload

--- client/src/plugins/index.ts
+++ client/src/plugins/index.ts
@@ -7,10 +7,9 @@
 import { loadFonts } from "./webfontloader";
 import vuetify from "./vuetify";
 import pinia from "../store";
-import router from "../router";
 import type { App } from "vue";
 
 export function registerPlugins(app: App) {
   loadFonts();
-  app.use(vuetify).use(router).use(pinia);
+  app.use(vuetify).use(pinia);
 }
